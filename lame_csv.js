fe79e7748c591967578939ea67abf066a8d9f896
Dmytro Yashkir <dima.yashkir@gmail.com>
2014-01-20 17:44:29 -0500

    more

--- lame_csv.js
+++ lame_csv.js
@@ -1,7 +1,31 @@
-var _ = require('underscrore');
+var _s = require('underscore');
 
 function lameCSV(str) {
-  return _.reduce(str.split("\n"), function(table, row) {
-    table.push(_.map(row.split(","), function(c) { return c.trim()}));
-    return table; }, []);
+  return _s.reduce(str.split("\n"), function(table, row) {
+    table.push(_s.map(row.split(","), function(c) { return c.trim()}));
+    return table; 
+  }, []);
 };
+var peopleTable = lameCSV("name,age,hair\nMerble,35,red\nBob,64,blonde");
+
+console.dir(peopleTable);
+
+function selectNames(table) {
+  return _s.rest(_s.map(table, _s.first));
+}
+
+function second(ar){
+  return ar[1];
+}
+
+function selectAges(table) {
+  return _s.rest(_s.map(table, second));
+}
+
+function nth(ar, index){
+  return ar[index];
+}
+function selectHairColor(table) {
+  return _s.rest(_s.map(table, function(row) {
+    return nth(row, 2); }));
+}
